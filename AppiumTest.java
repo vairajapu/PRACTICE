275cea0efdcec9191622c4743007334477851e64
vairajapu <34333272+vairajapu@users.noreply.github.com>
2020-12-04 13:55:02 +0530

    COMMIT

---
+++ AppiumTest.java
@@ -0,0 +1,18 @@
+package AugustBatch.August_Maven;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void OpenBrowser()
+	{
+		System.out.println("Opening Browser");
+	}
+	
+	@Test
+	public void closeBrowser()
+	{
+		System.out.println("Closing Browser");
+	}
+}
---
+++ SeleniumTest.java
@@ -0,0 +1,18 @@
+package AugustBatch.August_Maven;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void doLogin()
+	{
+		System.out.println("Executing Login Test");
+	}
+	
+	@Test
+	public void userRegistration()
+	{
+		System.out.println("Rehistering User");
+	}
+}
